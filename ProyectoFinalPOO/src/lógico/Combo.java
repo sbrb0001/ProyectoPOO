0351d822343e8e05d3b251b6aa176ca1cf09f1da
Jose Lopez <Jose Lopez@LAPTOP-N1GCNASO>
2023-03-30 16:31:45 -0400

    Clase Combo modificada con el atributo nombre(indentificador).

--- ProyectoFinalPOO/src/lógico/Combo.java
+++ ProyectoFinalPOO/src/lógico/Combo.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class Combo {
 	
-	
+	private String nombre; 
 	private ArrayList<Componente> losComponentes;
-
-	public Combo(ArrayList<Componente> losComponentes) {
+	
+	public Combo(ArrayList<Componente> losComponentes, String nombre) {
 		super();
 		this.losComponentes = new ArrayList<Componente>();
+		this.nombre = nombre;
 	}
 
 	public ArrayList<Componente> getLosComponentes() {
@@ -19,9 +20,14 @@ public class Combo {
 	public void setLosComponentes(ArrayList<Componente> losComponentes) {
 		this.losComponentes = losComponentes;
 	}
-	
-	
-	
-	
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
